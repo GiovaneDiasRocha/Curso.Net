592f74880f23dc35260f006c3e50d33dc4b7adb6
GiovaneDiasRocha <66972321+GiovaneDiasRocha@users.noreply.github.com>
2021-06-10 11:33:31 -0300

    Att

--- Course/Course/Produto.cs
+++ Course/Course/Produto.cs
@@ -3,28 +3,68 @@ namespace Course
 {
     public class Produto
     {
-        public string nomeDoProduto;
-        public double precoDoProduto;
-        public int quantidadeDoProduto;
+        private string _nomeDoProduto;
+        public double PrecoDoProduto {get; private set;}
+        public int QuantidadeDoProduto { get; private set; }
+        
+        
+
+        public Produto()
+        {
+            QuantidadeDoProduto = 0;
+        }
+
+        public Produto(string nome, double preco) : this()
+        {
+            _nomeDoProduto = nome;
+            PrecoDoProduto = preco;
+        }
+
+        public Produto(string nome, double preco, int quantidade) : this(nome, preco)
+        {
+            QuantidadeDoProduto = quantidade;
+        }
+
+        public string Nome
+        {
+            get { return _nomeDoProduto; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nomeDoProduto = value;
+                }
+            }
+        }
+
+        public double Preco
+        {
+            get { return PrecoDoProduto; }
+        }
+
+        public int Quantidade
+        {
+            get { return QuantidadeDoProduto; }
+        }
 
         public double valorTotalEmEstoque()
         {
-            return quantidadeDoProduto * precoDoProduto;
+            return QuantidadeDoProduto * PrecoDoProduto;
         }
 
         public void adicionarProdutos(int quant)
         {
-            quantidadeDoProduto += quant;
+            QuantidadeDoProduto += quant;
         }
 
         public void removerProdutos(int quant)
         {
-            quantidadeDoProduto -= quant;
+            QuantidadeDoProduto -= quant;
         }
 
         public override string ToString()
         {
-            return $"Dados do produto: {nomeDoProduto}, R${precoDoProduto.ToString("F2", CultureInfo.InvariantCulture)}, {quantidadeDoProduto} unidades, Total: R${valorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture)}.";
+            return $"Dados do produto: {_nomeDoProduto}, R${PrecoDoProduto.ToString("F2", CultureInfo.InvariantCulture)}, {QuantidadeDoProduto} unidades, Total: R${valorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture)}.";
         }
     }
 }
--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -7,68 +7,9 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            //Exercício área triângulo 
-
-            Triangulo x, y;
-            x = new Triangulo();
-            y = new Triangulo();
-
-            Console.Write("Digite o lado A do triângulo x: ");
-            x.a = double.Parse(Console.ReadLine());
-
-            Console.Write("Digite o lado B do triângulo x: ");
-            x.b = double.Parse(Console.ReadLine());
-
-            Console.Write("Digite o lado C do triângulo x: ");
-            x.c = double.Parse(Console.ReadLine());
-
-            Console.WriteLine();
-
-            Console.Write("Digite o lado A do triângulo y: ");
-            y.a = double.Parse(Console.ReadLine());
-
-            Console.Write("Digite o lado B do triângulo y: ");
-            y.b = double.Parse(Console.ReadLine());
-
-            Console.Write("Digite o lado C do triângulo y: ");
-            y.c = double.Parse(Console.ReadLine());
-
-            double areaX = x.Area();
-            double areaY = y.Area();
-
-            Console.WriteLine("Área do Triângulo X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
-            Console.WriteLine("Área do Triângulo Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
-
-            if (areaX > areaY)
-            {
-                Console.WriteLine("A maior área é: " + areaX.ToString("F4", CultureInfo.InvariantCulture));
-            }
-            else
-            {
-                Console.WriteLine("A maior área é: " + areaY.ToString("F4", CultureInfo.InvariantCulture));
-            }
-
-
-            //Exercício estoque produto
-            Produto produtoX;
-            produtoX = new Produto();
-
-            Console.WriteLine("Digite os dados do produto:");
-            Console.Write("Nome: ");
-            produtoX.nomeDoProduto = Console.ReadLine();
-            Console.Write("Preço: ");
-            produtoX.precoDoProduto = double.Parse(Console.ReadLine());
-            Console.Write("Quantidade no estoque: ");
-            produtoX.quantidadeDoProduto = int.Parse(Console.ReadLine());
-
-            Console.WriteLine(produtoX.ToString());
-            Console.Write("Digite o número de produtos a serem adicionados ao estoque: ");
-            produtoX.adicionarProdutos(int.Parse(Console.ReadLine()));
-            Console.WriteLine(produtoX.ToString());
-            Console.Write("Digite o número de produtos a serem removidos do estoque: ");
-            produtoX.removerProdutos(int.Parse(Console.ReadLine()));
-            Console.WriteLine(produtoX.ToString());
-
+            Produto p = new Produto("TV", 1500, 5);
+            p.Nome = "TV 4K";
+            Console.WriteLine(p.Nome+ ", " + p.Preco + ", " + p.Quantidade);
         }
     }
 }
